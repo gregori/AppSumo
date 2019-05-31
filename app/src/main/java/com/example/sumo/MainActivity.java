93503ceaf10a081360c8feec627dcf50c556c72a
mhenr <matheushhorongoso@gmail.com>
2019-05-30 21:32:20 -0300

    Criado layout tela de Parametros

--- app/src/main/java/com/example/sumo/MainActivity.java
+++ app/src/main/java/com/example/sumo/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.sumo.ui.main.SectionsPagerAdapter;
 
