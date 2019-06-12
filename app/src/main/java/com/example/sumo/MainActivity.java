4967a2a23b2db0afb088e4241539a074b468ebba
mhenr <matheushhorongoso@gmail.com>
2019-06-11 21:38:47 -0300

    Criado Validações de campos nulos dos botões(Ataque, Recover e Search)

--- app/src/main/java/com/example/sumo/MainActivity.java
+++ app/src/main/java/com/example/sumo/MainActivity.java
@@ -1,16 +1,9 @@
 package com.example.sumo;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-
 import com.example.sumo.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
