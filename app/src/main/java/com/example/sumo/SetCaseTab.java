8adb8400ae81347dbdfe1c2c8c405465c1a12dd4
Esdras Xavier <esdrasxa@gmail.com>
2019-08-26 20:21:20 -0300

    Changed the icon, changed the set case screen, now only has 10 set cases, no more is needed

--- app/src/main/java/com/example/sumo/SetCaseTab.java
+++ app/src/main/java/com/example/sumo/SetCaseTab.java
@@ -1,78 +1,60 @@
 package com.example.sumo;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
 import android.widget.Toast;
 
 public class SetCaseTab extends Fragment implements View.OnClickListener {
 
-    private Button btnSetCase1;
-    private Button btnSetCase2;
-    private Button btnSetCase3;
-    private Button btnSetCase4;
-    private Button btnSetCase5;
-    private Button btnSetCase6;
-    private Button btnSetCase7;
-    private Button btnSetCase8;
-    private Button btnSetCase9;
-    private Button btnSetCase10;
-    private Button btnSetCase11;
-    private Button btnSetCase12;
-    private Button btnSetCase13;
-    private Button btnSetCase14;
-    private Button btnSetCase15;
-    private Button btnSetCase16;
-
-    private TextView txvMessageControl;
     private Bluetooth bluetooth = new Bluetooth();
-    private String StrategyNumber = "";
-    private Boolean EstrategiaSetada = false;
+    private TableLayout buttonSpace;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.setcase_tab, container, false);
+        View view = inflater.inflate(R.layout.setcase_tab, container, false);
+        buttonSpace = view.findViewById(R.id.button_space);
 
-        btnSetCase1 = view.findViewById(R.id.btnSetCase1);
-        btnSetCase2 = view.findViewById(R.id.btnSetCase2);
-        btnSetCase3 = view.findViewById(R.id.btnSetCase3);
-        btnSetCase4 = view.findViewById(R.id.btnSetCase4);
-        btnSetCase5 = view.findViewById(R.id.btnSetCase5);
-        btnSetCase6 = view.findViewById(R.id.btnSetCase6);
-        btnSetCase7 = view.findViewById(R.id.btnSetCase7);
-        btnSetCase8 = view.findViewById(R.id.btnSetCase8);
-        btnSetCase9 = view.findViewById(R.id.btnSetCase9);
-        btnSetCase10 = view.findViewById(R.id.btnSetCase10);
-        btnSetCase11 = view.findViewById(R.id.btnSetCase11);
-        btnSetCase12 = view.findViewById(R.id.btnSetCase12);
-        btnSetCase13 = view.findViewById(R.id.btnSetCase13);
-        btnSetCase14 = view.findViewById(R.id.btnSetCase14);
-        btnSetCase15 = view.findViewById(R.id.btnSetCase15);
-        btnSetCase16 = view.findViewById(R.id.btnSetCase16);
-        txvMessageControl = view.findViewById(R.id.txvMessageControl);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(32, 8, 8, 32);
 
-        btnSetCase1.setOnClickListener(this);
-        btnSetCase2.setOnClickListener(this);
-        btnSetCase3.setOnClickListener(this);
-        btnSetCase4.setOnClickListener(this);
-        btnSetCase5.setOnClickListener(this);
-        btnSetCase6.setOnClickListener(this);
-        btnSetCase7.setOnClickListener(this);
-        btnSetCase8.setOnClickListener(this);
-        btnSetCase9.setOnClickListener(this);
-        btnSetCase10.setOnClickListener(this);
-        btnSetCase11.setOnClickListener(this);
-        btnSetCase12.setOnClickListener(this);
-        btnSetCase13.setOnClickListener(this);
-        btnSetCase14.setOnClickListener(this);
-        btnSetCase15.setOnClickListener(this);
-        btnSetCase16.setOnClickListener(this);
+
+        for (int i=0; i < 10; i++) {
+            Button a = new Button(view.getContext());
+            a.setText("Set case " + (i + 1));
+            a.setBackground(ContextCompat.getDrawable(view.getContext(), R.drawable.button_principal));
+            buttonSpace.addView(a, layoutParams);
+            a.setLayoutParams(layoutParams);
+            a.setTextColor(Color.parseColor("#DDDDDD"));
+
+            final int finalI = i;
+            a.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int strategy = finalI + 1;
+                    byte[] strategyData;
+
+                    try {
+                        strategyData = Protocol.intToByte(strategy);
+                        Log.i("Strategy tab", "Value: " + strategyData[0] + " - " + strategyData[1]);
+                        byte[] data = new byte[]{Protocol.SET_STRATEGY, strategyData[0], strategyData[1]};
+                        bluetooth.write(data);
+                        Toast.makeText(getActivity(), "A estratégia pré-definida '" + strategy + "' foi ativada!!!", Toast.LENGTH_LONG).show();
+                    } catch (Exception e) {
+                        Toast.makeText(getActivity(), "Erro ao setar estratégia. Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
 
         return view;
     }
@@ -80,56 +62,6 @@ public class SetCaseTab extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
-        int strategy = 0;
-        byte[] strategyData;
 
-        if (v == this.btnSetCase1) {
-            strategy = 1;
-        } else if (v == this.btnSetCase2) {
-            strategy = 2;
-        } else if (v == this.btnSetCase3) {
-            strategy = 3;
-        } else if (v == this.btnSetCase4) {
-            strategy = 4;
-        } else if (v == this.btnSetCase5) {
-            strategy = 5;
-        } else if (v == this.btnSetCase6) {
-            strategy = 6;
-        } else if (v == this.btnSetCase7) {
-            strategy = 7;
-        } else if (v == this.btnSetCase8) {
-            strategy = 8;
-        } else if (v == this.btnSetCase9) {
-            strategy = 9;
-        } else if (v == this.btnSetCase10) {
-            strategy = 10;
-        } else if (v == this.btnSetCase11) {
-            strategy = 11;
-        } else if (v == this.btnSetCase12) {
-            strategy = 12;
-        } else if (v == this.btnSetCase13) {
-            strategy = 13;
-        } else if (v == this.btnSetCase14) {
-            strategy = 14;
-        } else if (v == this.btnSetCase15) {
-            strategy = 15;
-        } else if (v == this.btnSetCase16) {
-            strategy = 16;
-        }
-
-
-        strategyData = Protocol.intToByte(strategy);
-        Log.i("Strategy tab", "Value: " + strategyData[0] + " - " + strategyData[1]);
-        byte[] data = new byte[] { Protocol.SET_STRATEGY, strategyData[0], strategyData[1] };
-        bluetooth.write(data);
-    }
-
-
-    private void ShowMessage(){
-        if(EstrategiaSetada) {
-            txvMessageControl.setText("Estratégia '"+StrategyNumber+ "' Enviada!!");
-            EstrategiaSetada = false;
-            Toast.makeText(this.getActivity(), "A estratégia pré-definida '" + StrategyNumber + "' foi ativada!!!", Toast.LENGTH_LONG).show();
-        }
     }
 }
